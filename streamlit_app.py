7216f88ee7e6d23e1de11026d2cee98fa660cd7c
Divyansh3021 <divyansh3021@gmail.com>
2023-08-12 11:50:59 +0530

    pdf reader module changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import fitz  # PyMuPDF
+# import fitz  # PyMuPDF
+import PyPDF2
 import index
 
 # Set up the Streamlit app
@@ -23,8 +24,8 @@ if uploaded_file is not None:
     st.subheader("Page {} of the Book:".format(current_page + 1))
     
     if uploaded_file.type == "application/pdf":
-        pdf_document = fitz.open(stream=uploaded_file.read())
-        num_pages = pdf_document.page_count
+        pdf_document = PyPDF2.PdfReader(uploaded_file)
+        num_pages = len(pdf_document.pages)
         if current_page >= num_pages:
             current_page = num_pages - 1
             st.session_state.current_page = current_page
@@ -33,7 +34,8 @@ if uploaded_file is not None:
         # page_number = pdf_document.page_numbers[current_page]
         
         # Use pdf_document.get_page_text() to get the page text with formatting
-        page_text = pdf_document.load_page(current_page).get_text()
+        page = pdf_document.pages[current_page]
+        page_text = page.extract_text()
         summarized_text += summarizer.summarize(page_text)
         
         # Display the page number and page text
